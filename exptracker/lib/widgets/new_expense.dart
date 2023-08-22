236a3ba91f8e107d51ea6d0c8d10bcc89fd3a204
Quentin <synkx@hotmail.fr>
2023-08-22 08:53:59 +0200

    begin meals app

--- exptracker/lib/widgets/new_expense.dart
+++ exptracker/lib/widgets/new_expense.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:exptracker/models/expense.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class NewExpense extends StatefulWidget {
@@ -218,13 +221,24 @@ class _NewExpenseState extends State<NewExpense> {
     });
   }
 
-  void _submitExpenseData() {
-    final enteredAmount = double.tryParse(_amountController.text);
-    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
-
-    if (_titleController.text.trim().isEmpty ||
-        amountIsInvalid ||
-        _selectedDateTime == null) {
+  void _showDialog() {
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+          context: context,
+          builder: (ctx) => CupertinoAlertDialog(
+                title: const Text("Invalid input."),
+                content: const Text(
+                    "Please make sure a valid title, amount, date and category was entered."),
+                actions: [
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(ctx);
+                    },
+                    child: const Text("Okay"),
+                  )
+                ],
+              ));
+    } else {
       showDialog(
           context: context,
           builder: (ctx) => AlertDialog(
@@ -240,6 +254,17 @@ class _NewExpenseState extends State<NewExpense> {
                   )
                 ],
               ));
+    }
+  }
+
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDateTime == null) {
+      _showDialog();
       return;
     }
 
